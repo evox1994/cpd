4b86b2b010dcb8f61cf12693d088052a3c930ca2
evox1994 <dmitrybatukhtin@gmail.com>
2019-08-27 12:29:03 +0700

    sfssvf

--- js/catalog.js
+++ js/catalog.js
@@ -10,4 +10,24 @@ $(document).ready(function(){
 	}
 	checkId();
 
+	$(document).on('click','.filter-btn',function(){
+		var el = $(this).attr('href');
+		$(el).addClass('active');
+		$('body').addClass('no-scroll');
+		return false;
+	});
+
+	$(document).on('click','.filter-close',function(){
+		var el = $(this).attr('href');
+		$(el).removeClass('active');
+		$('body').removeClass('no-scroll');
+		return false;
+	});
+
+	$(document).on('click','.filter button[type="submit"]',function(){
+		$(this).parents('.filter').removeClass('active');
+		$('body').removeClass('no-scroll');
+		return false;
+	});
+
 });
